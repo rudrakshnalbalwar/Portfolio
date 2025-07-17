ccaec5fdafd2c44fba1d231b8768431f0cf00e68
Rudraksh Gajanan Nalbalwar <nalbalwarrudraksh@gmail.com>
2025-07-17 20:30:44 +0530

    updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -24,7 +24,7 @@ export const AboutSection = () => {
                                 Get In Touch
                             </a>
                             <a 
-                                href="/Rudraksh_CV.pdf" 
+                                href="/Rudraksh_Nalbalwar_Resume_SDE.pdf" 
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-color duration-300"
                             >
                                 Download CV
